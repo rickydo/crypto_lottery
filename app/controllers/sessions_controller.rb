77f4eba1408043e2ad05d626875086865e3d780a
rickydo <g3tdo@yahoo.com>
2014-11-08 17:58:24 -0600

    included sessions helper into application controller, sign up and sign out function works but sign in function doesnt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,29 @@
 class SessionsController < ApplicationController
 
-	def new
-
-	end 
+	def new 
+		if session[:user_id]
+			redirect_to root_path
+		end
+	end
 
 	def create
+		@user = User.find_by(email: params[:email])
 
+		if @user
+			if @user.authenticate(params[:password])
+				session[:user_id] = @user.id
+				redirect_to root_path
+			else 
+				redirect_to root_path
+			end
+		else
+			flash[:error] = "Incorrect Username or password"
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_path
 	end
 end
