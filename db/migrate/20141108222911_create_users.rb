28f0fc299aef508767819ca8223c8e2c398dbe53
rickydo <g3tdo@yahoo.com>
2014-11-08 16:47:54 -0600

    user now only has email and password

--- db/migrate/20141108222911_create_users.rb
+++ db/migrate/20141108222911_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string			:first_name
-    	t.string			:last_name
     	t.string			:email
     	t.string			:coin_address
+    	t.string			:password_digest
       t.timestamps
     end
   end
