08d3285ff94850d8134180ffa129fabf6b8e41fe
rickydo <g3tdo@yahoo.com>
2014-11-08 16:52:19 -0600

    added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :email, presence: :true 
 	validates :email, uniqueness: :true
+	validates :email, format { with: /\S{3,}@\S{2,}/ }
 end
